8684c4e34dc005614f0509c261d81ca23591ad9e
Mr. Habib <78511534+mr-habib@users.noreply.github.com>
2021-02-22 20:52:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 # Firstname, L
 # mm/dd/yyyy
 # Creative and descriptive title
+
+## Note: To run the teacher's .pyc file, go into the .replit file and change the line that says: run = "python main.py" to run = "python mr_habib_calculator.cpython-38.pyc"
+
