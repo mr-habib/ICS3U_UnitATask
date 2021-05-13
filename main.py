a1b1d24729a8339f40da820e9728523f47c2a08d
Mr. Habib <sirmrhabib@gmail.com>
2021-05-13 11:20:44 -0400

    Modified assignment in Q4_2021

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-# Firstname, L
+# Lastname, Firstname
 # mm/dd/yyyy
 # Creative and descriptive title
 
 ## Note: To run the teacher's .pyc file, go into the .replit file and change the line that says: run = "python main.py" to run = "python mr_habib_calculator.cpython-38.pyc"
+
+def eval_math(expr):
+    pass # Remove This
+
+def main():
+    pass # Remove This
+
+if __name__ == '__main__':
+    main()
 
---
+++ main_test.py
@@ -0,0 +1,33 @@
+from main_solution import eval_math
+from random import randint, choice
+
+def test_eval_math_simple_add():
+    assert(eval_math("10 + 6") == "10 + 6 = 16")
+
+def test_eval_math_simple_sub():
+    assert(eval_math("11 - 6") == "11 - 6 = 5")
+
+def test_eval_math_simple_sub2():
+    assert(eval_math("6 - 12") == "6 - 12 = -6")
+
+def test_eval_math_simple_mult():
+    assert(eval_math("8 * 9") == "8 * 9 = 72")
+
+def test_eval_math_simple_div():
+    assert(eval_math("11 / 5") == "11 / 5 = 2.2")
+
+def test_eval_math_simple_pow():
+    assert(eval_math("3 ^ 4") == "3 ^ 4 = 81")
+
+def test_eval_math_random_1():
+    num1 = randint(0, 1000)
+    num2 = randint(0, 1000)
+    list_of_operators = ['+', '-', '*', '/', '^']
+    op = choice(list_of_operators)
+    str_input = f"{num1} {op} {num2}"
+    if op == '^':
+        str_input = f"{num1} ^ {num2}"
+    ans = eval(str_input)
+    expected = f"{str_input} = {ans}"
+    assert(eval_math(str_input) == expected)
+
